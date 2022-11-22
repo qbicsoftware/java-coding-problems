5dd4e66f023708240d76b8babadff6bb06436da9
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2022-11-22 10:18:30 +0100

    implement bubble sort for int arrays

--- date-and-time/src/main/java/life/qbic/Problem99.java
+++ date-and-time/src/main/java/life/qbic/Problem99.java
@@ -1,5 +1,6 @@
 package life.qbic;
 
+import java.util.Arrays;
 import java.util.Random;
 
 /**
@@ -13,9 +14,31 @@ public class Problem99 {
     testIntSorting = fill(testIntSorting);
     testObjectSorting = fill(testObjectSorting);
     //TODO sort test array
+    System.out.println("testIntSorting = " + Arrays.toString(testIntSorting));
+    bubbleSort(testIntSorting);
+    System.out.println("testIntSorting (bubble sort)= " + Arrays.toString(testIntSorting));
+    testIntSorting = fill(testIntSorting);
+    System.out.println("testIntSorting = " + Arrays.toString(testIntSorting));
+    Arrays.sort(testIntSorting);
+    System.out.println("testIntSorting (Arrays.sort)= " + Arrays.toString(testIntSorting));
+
+
     //TODO sort testObjectSorting
     //TODO shuffle test integer array
     //TODO shuffle test object array
+  }
+
+  public static void bubbleSort(int[] toBeSorted) {
+    for (int outerIndex = 0; outerIndex < toBeSorted.length; outerIndex++) {
+      int remainingUnsorted = toBeSorted.length - outerIndex;
+      for (int i = 0; i < remainingUnsorted - 1; i++) {
+        if (toBeSorted[i + 1] < toBeSorted[i]) {
+          int temp = toBeSorted[i + 1];
+          toBeSorted[i + 1] = toBeSorted[i];
+          toBeSorted[i] = temp;
+        }
+      }
+    }
   }
 
   private static int[] fill(int[] test) {
