7861eb43206c9612fd9251b2d60aafecda21ac90
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2022-12-20 10:17:33 +0100

    add main class

---
+++ AdventOfCode22/src/main/java/life/qbic/day8/Main.java
@@ -0,0 +1,16 @@
+package life.qbic.day8;
+
+/**
+ * <b>short description</b>
+ *
+ * <p>detailed description</p>
+ *
+ * @since <version tag>
+ */
+public class Main {
+
+  public static void main(String[] args) {
+
+  }
+
+}
