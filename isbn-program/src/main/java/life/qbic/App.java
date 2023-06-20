c50fe590cc95a99614c58ca94d07670bc796460b
Tobias Koch <kochtobi@users.noreply.github.com>
2023-06-20 08:45:30 +0200

    parse all and print output

--- isbn-program/src/main/java/life/qbic/App.java
+++ isbn-program/src/main/java/life/qbic/App.java
@@ -1,5 +1,10 @@
 package life.qbic;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,24 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+            List<String> isbn10List = Files.readAllLines(Path.of("isbn10.txt"));
+            int failedCounter = 0;
+            int succeededCounter = 0;
+            for (String isbn10 : isbn10List) {
+                try {
+                    Isbn10.parse(isbn10);
+                    succeededCounter++;
+                } catch (IllegalArgumentException e) {
+                    failedCounter++;
+                    System.out.println("failed parsing " + isbn10);
+                }
+            }
+            System.out.println("succeededCounter = " + succeededCounter);
+            System.out.println("failedCounter = " + failedCounter);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
